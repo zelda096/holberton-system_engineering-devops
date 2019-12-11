97725815d00f4521ed8011be4350ae12bcf76d69
zelda096 <miguelangel697@gmail.com>
2019-12-11 00:00:16 -0500

    add

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# ssh_config
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+-> file_line { 'change pass id to /etc/ssh/ssh_config':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
+file_line { 'change IdentutyFile to /etc/ssh/ssh_config':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/holberton',
+}
