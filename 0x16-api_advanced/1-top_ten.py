b5f3128704386730ee658a89a311cd6eba4e558e
zelda096 <miguelangel697@gmail.com>
2020-02-06 16:10:11 -0500

    task1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Find the number of subs"""
+import requests
+
+
+def top_ten(subreddit):
+    """ show the top 10 posts in a subreddit """
+    try:
+        # Change the user agent
+        headers = {'User-Agent': 'cmmolanos'}
+        payload = {'t': 'all', 'limit': '10'}
+        request = requests.get('https://api.reddit.com/r/{}/hot'.
+                               format(subreddit), headers=headers,
+                               params=payload)
+        top_posts = request.json()
+
+        for post in top_posts['data']['children']:
+            print(post['data']['title'])
+
+    except:
+        print("None")
